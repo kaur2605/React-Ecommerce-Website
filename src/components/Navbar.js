41acc029b8f0e63e68b2241165f32cb00cbb37c8
Gurdeep Kaur <gurdeep.kaur@karnovgroup.com>
2020-11-18 14:23:23 +0100

    style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,32 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-<<<<<<< HEAD
-=======
-import log from "../log.svg";
->>>>>>> 11c4d15a9b7a0d6990cc3cb2923724aeb9147983
+import {Link} from "react-router-dom"
 import styled from "styled-components";
 import ButtonBox from "./Button";
+import log from "../log.png"
 
 export default class Navbar extends Component {
   render() {
     return (
       <Navwrapper className="navbar navbar-expand-sm  bg-primary px-sm-5">
-        <Link to="/" className= "logo">
-          <span>TheShopSmart</span>
+        <Link to="/" className="logo">
+          <img src={log} />
+          <span className = "logo">TheShopSmart</span>
         </Link>
-        <Link to="/" className="nav-link text-dark">Home</Link>
+        <Link to="/" className="nav-link text-dark">
+          Home
+        </Link>
 
         <ul className="navbar nav align-items-center">
           <li className="nav-item ml-1">
-
             <Link to="/Products" className="nav-link text-dark">
               {" "}
               Products
             </Link>
-
           </li>
         </ul>
-        <Link to="/About" className="nav-link text-dark">About Us</Link>
+        <Link to="/About" className="nav-link text-dark">
+          About Us
+        </Link>
 
         <Link to="/Cart" className="ml-auto">
           <ButtonBox>
@@ -36,7 +36,7 @@ export default class Navbar extends Component {
           </ButtonBox>
         </Link>
       </Navwrapper>
-    );
+    )
   }
 }
 const Navwrapper = styled.nav`
